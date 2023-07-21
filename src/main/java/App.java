8714052e5cd7f2ff94f43e8a3b1f8de5c754df52
rprasadd <raghuveer.prasaddr@anko.com>
2023-07-21 20:44:32 +0530

    updated code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 public class App
 {
     public static void main( String[] args )
     {
+        //Instantiating the File class
+        File file = new File(".github\output");
+        //Instantiating the PrintStream class
+        PrintStream stream = new PrintStream(file);
+        System.setOut(stream);
+        //Printing values to file    
+           
         System.out.println("I am your awesome Java application!");
 
         DeepThought ourSuperComputer = new DeepThought();
